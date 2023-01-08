9cc7aedc7164aea0ea8c0532fd607b5f3c3fafb9
ybwi0912 <ybwi0912@naver.com>
2023-01-08 15:03:33 +0900

    Do it! Java chapter 1

---
+++ src/main/java/org/example/hello/HelloJava.java
@@ -0,0 +1,12 @@
+package org.example.hello;
+
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java");
+        // 한 줄 주석 연습
+
+        /*
+        * 여러 줄 주석 표시
+        * */
+    }
+}
