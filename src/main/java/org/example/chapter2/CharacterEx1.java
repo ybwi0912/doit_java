1a44e609c416633dfe92830909b4e99c44a8e942
ybwi0912 <ybwi0912@naver.com>
2023-01-08 15:53:36 +0900

    Do it! Java chapter 2

---
+++ src/main/java/org/example/chapter2/CharacterEx1.java
@@ -0,0 +1,16 @@
+package org.example.chapter2;
+
+public class CharacterEx1 {
+    public static void main(String[] args) {
+        char ch1 = 'A';
+        System.out.println("ch1 = " + ch1);
+        System.out.println((int)ch1);
+
+        char ch2 = 66;
+        System.out.println("ch2 = " + ch2);
+
+        int ch3 = 67;
+        System.out.println("ch3 = " + ch3);
+        System.out.println((char)ch3);
+    }
+}
