9048bbf4d10f765c9406416f648738567248abe8
ybwi0912 <ybwi0912@naver.com>
2023-01-25 14:14:30 +0900

    Do it! Java chapter 8

---
+++ src/main/java/org/example/chapter8/polymorphism/VIPCustomer.java
@@ -0,0 +1,28 @@
+package org.example.chapter8.polymorphism;
+
+public class VIPCustomer extends Customer {
+    private int agentID;
+    double saleRatio;
+
+    public VIPCustomer(int customerID, String customerName, int agentID){
+        super(customerID, customerName);
+        customerGrade = "VIP";
+        bonusRatio = 0.05;
+        saleRatio = 0.1;
+        this.agentID = agentID;
+    }
+
+    public int getAgentID(){
+        return agentID;
+    }
+
+    @Override
+    public int calcPrice(int price){
+        bonusPoint += price * bonusRatio;
+        return price - (int)(price * saleRatio);
+    }
+
+    public String showCustomerInfo(){
+        return super.showCustomerInfo() + " 담당 상담원 번호는 " + agentID + "입니다.";
+    }
+}
