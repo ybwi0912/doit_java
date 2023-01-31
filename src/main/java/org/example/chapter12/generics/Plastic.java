ac9e4a92b7fb630f964c5d7ce9996aeb12209f52
ybwi0912 <ybwi0912@naver.com>
2023-01-31 22:11:40 +0900

    Do it! Java chapter 12

---
+++ src/main/java/org/example/chapter12/generics/Plastic.java
@@ -0,0 +1,11 @@
+package org.example.chapter12.generics;
+
+public class Plastic {
+    public void doPrinting(){
+        System.out.println("Plastic 재료로 출력합니다.");
+    }
+
+    public String toString(){
+        return "재료는 Plastic입니다.";
+    }
+}
