85a5a7ba0c820d76e0be6f430f238e29938c880c
ybwi0912 <ybwi0912@naver.com>
2023-02-02 14:48:48 +0900

    Do it! Java chapter 12

---
+++ src/main/java/org/example/chapter12/collection/hashset/MemberHashSetTest.java
@@ -0,0 +1,22 @@
+package org.example.chapter12.collection.hashset;
+
+import org.example.chapter12.collection.Member;
+
+public class MemberHashSetTest {
+    public static void main(String[] args) {
+        MemberHashSet memberHashSet = new MemberHashSet();
+
+        Member memberLee = new Member(1001, "이지원");
+        Member memberSon = new Member(1002, "손민국");
+        Member memberPark = new Member(1003, "박서준");
+
+        memberHashSet.addMember(memberLee);
+        memberHashSet.addMember(memberSon);
+        memberHashSet.addMember(memberPark);
+        memberHashSet.showAllMember();
+
+        Member memberHong = new Member(1003, "홍길동");
+        memberHashSet.addMember(memberHong);
+        memberHashSet.showAllMember();
+    }
+}
---
+++ src/main/java/org/example/chapter12/collection/treeset/MemberTreeSetTest.java
@@ -0,0 +1,22 @@
+package org.example.chapter12.collection.treeset;
+
+import org.example.chapter12.collection.Member;
+
+public class MemberTreeSetTest {
+    public static void main(String[] args) {
+        MemberTreeSet memberTreeSet = new MemberTreeSet();
+
+        Member memberPark = new Member(1003, "박서준");
+        Member memberLee = new Member(1001, "이지원");
+        Member memberSon = new Member(1002, "손민국");
+
+        memberTreeSet.addMember(memberLee);
+        memberTreeSet.addMember(memberSon);
+        memberTreeSet.addMember(memberPark);
+        memberTreeSet.showAllMember();
+
+        Member memberHong = new Member(1003, "홍길동");
+        memberTreeSet.addMember(memberHong);
+        memberTreeSet.showAllMember();
+    }
+}
