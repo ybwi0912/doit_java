61a4461c43cd36692dcda9a12e5eee2c35cb71db
ybwi0912 <ybwi0912@naver.com>
2023-02-03 15:30:09 +0900

    Do it! Java chapter 14

---
+++ src/main/java/org/example/chapter14/exception/ThrowsException.java
@@ -0,0 +1,23 @@
+package org.example.chapter14.exception;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class ThrowsException {
+    public Class loadClass(String fileName, String className) throws FileNotFoundException, ClassNotFoundException {
+        FileInputStream fis = new FileInputStream(fileName);
+        Class c = Class.forName(className);
+        return c;
+    }
+
+    public static void main(String[] args) {
+        ThrowsException test = new ThrowsException();
+        try {
+            test.loadClass("a.txt", "java.lang.String");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
